edf53c6781abbcdf0e7515c4d5a357adbe95ffbe
nialov <nikolasovaskainen@hotmail.com>
2021-03-20 23:47:50 +0200

    add more to test

--- tests/test_myscript.py
+++ tests/test_myscript.py
@@ -5,7 +5,7 @@ Tests for mypackage.myscript.
 import pytest
 from pathlib import Path
 from typing import List
-from mypackage.myscript import myfunction
+from mypackage.myscript import myfunction, do_math
 
 
 @pytest.mark.parametrize(
@@ -21,3 +21,11 @@ def test_myfunction(directory_path: Path, assume_result: List[str]):
     """
     result = myfunction(directory_path=directory_path)
     assert all([assume in str(result) for assume in assume_result])
+
+
+def test_do_math():
+    """
+    Test do_math.
+    """
+    result = do_math([1.0, 56.0, 345345.1, 45.436])
+    assert isinstance(result, tuple)
